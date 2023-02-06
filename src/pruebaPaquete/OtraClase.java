24c8cfb1b32cd556dcad02c2409ec13373d1b240
elena <2019unipec@gmail.com>
2023-02-06 16:27:00 +0100

    Añadida la clase OtraClase.java

---
+++ src/pruebaPaquete/OtraClase.java
@@ -0,0 +1,16 @@
+package pruebaPaquete;
+
+public class ClasePrueba {
+
+	public static void main(String[] args) {
+
+		int [][] numeros = new int[5][2];
+
+		for (int i=0; i<numeros.length; i++){
+			for (int j=0; j<numeros[i].length; j++){
+				numero[i][j]=i;
+			}
+		}
+
+	}
+}
